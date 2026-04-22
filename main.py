082c24f51366164f793143ec1bd8f291cb1718db
Stephanie Vaccaro <stephanie.vaccaro523@gmail.com>
2026-04-22 15:54:46 -0500

    Update Colors

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ app = Flask(__name__,template_folder='templates', static_folder='static')
 
 @app.route("/")
 def index():
+    return redirect("/the-programming-dragon")
+
+@app.route("/the-programming-dragon")
+def programming_dragon():
     return "<p>Hello, World 2!</p>"
