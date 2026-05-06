92685db5d656f6cb20eb24c36dbec2faa710bf75
ProgrammingDragon523 <stephanie.vaccaro523@gmail.com>
2026-05-06 15:02:39 -0500

    Adding Resume Page Skeleton

--- main.py
+++ main.py
@@ -2,10 +2,48 @@ from flask import Flask, redirect, render_template
 
 app = Flask(__name__,template_folder='templates', static_folder='static')
 
+global SLIDE
+SLIDE = 0
+
+# Main Page
 @app.route("/")
 def index():
-    return redirect("/the-programming-dragon")
+    return redirect("/the-programming-dragon/welcome")
 
-@app.route("/the-programming-dragon")
-def programming_dragon():
+@app.route("/the-programming-dragon/welcome", methods=["POST", "GET"])
+def welcome():
     return render_template("welcome.html")
+
+@app.route("/the-programming-dragon/tavern")
+def tavern():
+    return render_template("choose_adventurer.html")
+
+# Adventurer Pages
+@app.route("/the-programming-dragon/tavern/<adventurer>")
+def adventurer_overview(adventurer):
+    return render_template("adventurer_overview.html")
+
+@app.route("/the-programming-dragon/tavern/<adventurer>/past-adventures")
+def past_adventures(adventurer):
+    return render_template("past_adventures.html")
+
+@app.route("/the-programming-dragon/tavern/<adventurer>/skills-and-powers")
+def skills_and_powers(adventurer):
+    return render_template("skills_and_powers.html")
+
+@app.route("/the-programming-dragon/tavern/<adventurer>/credentials")
+def credentials(adventurer):
+    return render_template("credentials.html")
+
+@app.route("/the-programming-dragon/tavern/<adventurer>/spell-scrolls")
+def spell_scrolls(adventurer):
+    return render_template("spell_scrolls.html")
+
+@app.route("/the-programming-dragon/tavern/<adventurer>/side-quests")
+def side_quests(adventurer):
+    return render_template("side_quests.html")
+
+# Sending Stone
+@app.route("/the-programming-dragon/tavern/cast-sending-stone")
+def cast_sending_stone(adventurer):
+    return render_template("cast_sending_stone.html")
---
+++ static/js/loadResume.js
@@ -0,0 +1,57 @@
+import stephanieData from "../data/stephanie_resume.json" with { type: "json" };
+
+function getExperience(firstProJob){
+    const firstJobMonthString = firstProJob.split(" ")[0]
+    const firstJobYears = firstProJob.split(" ")[1]
+    const firstJobMonthNum= new Date(`${firstJobMonthString} 1, ${firstJobYears}`).getMonth() + 1;
+    
+    const currentMonth = new Date().getMonth()+1
+    const currentYear = new Date().getFullYear()
+    
+    let yearsExp = 0
+    let monthExp = 0
+
+    if(firstJobMonthNum === currentMonth) {
+        yearsExp = currentYear - firstJobYears
+    }   
+    else if(firstJobMonthNum < currentMonth){
+        yearsExp = currentYear - firstJobYears - 1
+        monthExp = currentMonth - firstJobMonthNum
+    }
+    else{
+        yearsExp = currentYear - firstJobYears
+        monthExp = currentMonth
+    }
+    return {
+        monthExp:monthExp,
+        yearsExp:yearsExp
+    }
+}
+
+function loadResumeData(data){
+    /* Load in Name and Picture */
+    const firstName = data.personal_info.name.first
+    const firstNameLower = firstName.toLowerCase()
+    const fullName = firstName + " " + data.personal_info.name.last
+    const picture = new URL(`static/images/${firstName}.jpg`, window.location.origin);
+
+    /* Set HTML Values */
+    document.getElementById(`${firstNameLower}Name`).innerHTML=fullName
+    document.getElementById(`${firstNameLower}Title`).innerHTML=data.personal_info.dnd_title
+    document.getElementById(`${firstNameLower}Img`).src = picture
+    document.getElementById(`${firstNameLower}FactOne`).innerHTML=data.personal_info.job_style
+    document.getElementById(`${firstNameLower}FactTwo`).innerHTML=data.education.degrees[0].degree
+
+    /* Calculate and Set Years of Experience */
+    const experience = getExperience(data.experience.first_professional_job_start)
+    if(experience.monthExp>0){
+        document.getElementById(`${firstNameLower}FactThree`).innerHTML=`${experience.yearsExp} years ${experience.monthExp} months industry experience`
+    } else {
+        document.getElementById(`${firstNameLower}FactThree`).innerHTML=`${experience.yearsExp} years industry experience`
+    }
+
+    /* Set Button Value */
+    document.getElementById(`approach${firstName}`).innerHTML=`Approach ${firstName}`
+}
+
+loadResumeData(stephanieData)
