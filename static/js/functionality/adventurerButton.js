19e1edfbfe4e99a1db543c630564bd8800fba98c
Louise <louise.allen81@gmail.com>
2026-09-02 21:12:08 +0100

    Updated style on stylecss and welcomehtml

--- static/js/functionality/adventurerButton.js
+++ static/js/functionality/adventurerButton.js
@@ -8,7 +8,7 @@ function loadInitialText(adventurer){
     console.log(`SessionStorage: ${visited}`)
 
     if (visited.includes(adventurer)){
-        document.getElementById("introText").hidden="hidden"
+        document.getElementById("Narrator").hidden="hidden"
         document.getElementById("adventurerSpeech").innerText = "Welcome back, what can I help you with?";
         document.getElementById("continueAdventurerSpeech").hidden = "hidden"
         document.getElementById("exploreButtons").removeAttribute("hidden");
@@ -17,20 +17,20 @@ function loadInitialText(adventurer){
         visited.push(adventurer)
         console.log(`You've visited: ${visited}`)
         sessionStorage.setItem("visited", JSON.stringify(visited));
-        document.getElementById("introText").innerText = "You approach the young woman seated at a corner table, papers scattered in front of her.  A plate of brownie crumbs beside her, a small glass with little milk left in, and a small mechanical dragon perched on her shoulder are her companions as she continues to ponder her work. Sensing your presence she greets you."
+        document.getElementById("Narrator").innerText = "You approach the young woman seated at a corner table, papers scattered in front of her.  A plate of brownie crumbs beside her, a small glass with little milk left in, and a small mechanical dragon perched on her shoulder are her companions as she continues to ponder her work. Sensing your presence she greets you."
         document.getElementById("adventurerSpeech").innerText = "Well met, traveler. By your presence, I assume you need a developer?"
     } else {
         visited.push(adventurer)
         console.log(`You've visited: ${visited}`)
         sessionStorage.setItem("visited", JSON.stringify(visited));
-        document.getElementById("introText").innerText = "Louise's Intro Here"
+        document.getElementById("Narrator").innerText = "Louise's Intro Here"
         document.getElementById("adventurerSpeech").innerText = "Louise's Text Here"
     }
 }
 
 function changeText() {
     if (step === 0) {
-        document.getElementById("introText").hidden="hidden"
+        document.getElementById("Narrator").hidden="hidden"
         document.getElementById("adventurerSpeech").innerText = "Let me tell you a story or two...";
         document.getElementById("continueAdventurerSpeech").hidden = "hidden"
         document.getElementById("exploreButtons").removeAttribute("hidden");
--- static/js/functionality/introButton.js
+++ static/js/functionality/introButton.js
@@ -2,7 +2,7 @@ let step = 0
 
 function changeText() {
     if (step === 0) {
-        document.getElementById("introText").hidden="hidden"
+        document.getElementById("Narrator").hidden="hidden"
         document.getElementById("barkeepSpeech").innerText = "What can I get you? Water? Tea? Ale? Hmm?";
         step++;
     }
